521beb4677738db792118bee3b83974b704cade5
Tommy Yang <yt940928@gmail.com>
2018-11-01 17:37:06 -0700

    frontend starter code
    Vue + Bootstrap + Cypress

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-navbar class="mb-5" toggleable="md" type="dark" variant="info">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="/">CSE 5A Course Companion</b-navbar-brand>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item to="/">Home</b-nav-item>
+      </b-navbar-nav>
+      <!-- Right aligned nav items
+      <b-navbar-nav class="ml-auto">
+      </b-navbar-nav>
+      -->
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {},
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
