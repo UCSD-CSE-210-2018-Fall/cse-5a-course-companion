774bf65bbd4f47e94502f7e05ebda4cb90871274
Tommy Yang <yt940928@gmail.com>
2018-11-05 14:40:36 -0800

    add navbar item

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -4,7 +4,8 @@
     <b-navbar-brand href="/">CSE 5A Course Companion</b-navbar-brand>
     <b-collapse is-nav id="nav_collapse">
       <b-navbar-nav>
-        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item to="/" exact>Home</b-nav-item>
+        <b-nav-item to="/quiz">Quiz</b-nav-item>
       </b-navbar-nav>
       <!-- Right aligned nav items
       <b-navbar-nav class="ml-auto">
