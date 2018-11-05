96819c09e7af01fd58dbb5e26cf0205080d3d416
Chengchen Zhang <doublechenn@gmail.com>
2018-11-04 21:01:47 -0800

    Added New Multiple Choice Questions

--- frontend/src/Quiz.vue
+++ frontend/src/Quiz.vue
@@ -2,12 +2,23 @@
   <div>
     <h2>Quiz prototype</h2>
     <b-row class="justify-content-center">
-      <b-col md="6">
+   
+       <b-col md="6">
         <MultipleChoice></MultipleChoice>
+      </b-col>
+       <b-col md="6">
+        <MultipleChoice_2></MultipleChoice_2>
+      </b-col>
+       <b-col md="6">
+        <MultipleChoice_3></MultipleChoice_3>
+      </b-col>
+       <b-col md="6">
+        <MultipleChoice_4></MultipleChoice_4>
       </b-col>
       <b-col md="6">
         <FillBlank></FillBlank>
       </b-col>
+     
     </b-row>
   </div>
 </template>
@@ -15,6 +26,9 @@
 <script>
 import MultipleChoice from "./components/MultipleChoice.vue"
 import FillBlank from "./components/FillBlank.vue"
+import MultipleChoice_2 from "./components/MultipleChoice_2.vue"
+import MultipleChoice_3 from "./components/MultipleChoice_3.vue"
+import MultipleChoice_4 from "./components/MultipleChoice_4.vue"
 
 export default {
   name: "Quiz",
@@ -23,7 +37,11 @@ export default {
   },
   components: {
     MultipleChoice,
+    MultipleChoice_2,
+    MultipleChoice_3,
+    MultipleChoice_4,
     FillBlank
+    
   },
   methods: {}
 }
