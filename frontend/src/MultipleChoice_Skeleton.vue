11d1be010d4679903e2c01c23d0010e88fafedb0
Chengchen Zhang <doublechenn@gmail.com>
2018-11-04 21:00:22 -0800

    Added Multiple Choice Questions
    Added new multiple choice questions as well as a skeleton for new questions objects can be quickly created from.

---
+++ frontend/src/MultipleChoice_Skeleton.vue
@@ -0,0 +1,118 @@
+<template>
+
+    <div class="card my-3">
+      <b-card header="<strong>Multiple Choice</strong>">
+        <div>
+          <div class="mb-3" v-html="question"></div>
+        </div>
+        <b-list-group v-for="(choice, index) in choices" :key="index">
+          <label class="list-group-item list-group-item-action" :for="`choice${choices}${index}`" :class="{'active':chosen === index}">
+            <input class="d-none" type="radio" name="options" :id="`choice${choices}${index}`" v-model="chosen" :value="index">{{choice}}
+          </label>
+        </b-list-group>
+        <b-button :disabled="submitDisabled" @click="onSubmit" variant="outline-primary" class="my-3">Submit</b-button>
+        <b-alert :show="showAlert" class="my-3" :variant="correct ? 'success' : 'warning'">
+          {{msg}}
+        </b-alert>
+      </b-card>
+  </div>
+</template>
+
+<script>
+/* const variable_Question_Header = ["Question Number", "Question", "Question content"];
+*  const variable_Question_Choices = ["Answer 1;","Answer 2","Answer 3","Answer 4"];
+*  const variable_Responses = ["Response 1","Response 2","Response 3","Response 4"];
+*  const correct_Answer = 0;
+*/
+const variable_Question_Header = [
+    "Question Number", 
+    "Question", 
+    "Question content"
+];
+const variable_Question_Choices = [
+    "Answer 1;",
+    "Answer 2",
+    "Answer 3",
+    "Answer 4"
+];
+const variable_Responses = [
+    "Response 1",
+    "Response 2",
+    "Response 3",
+    "Response 4"
+];
+const correct_Answer = 0;
+
+
+export default {
+  name: variable_Question_Header[0],
+  props: {
+    question: {
+      type: String,
+      required: true,
+      default: "<p><strong>" + variable_Question_Header[1] + "</strong></p>" +
+        "<code>" + variable_Question_Header[2] + "</code>"
+    },
+    choices: {
+      type: Array,
+      required: true,
+      default: function() {
+        return [
+          variable_Question_Choices[0],
+          variable_Question_Choices[1],
+          variable_Question_Choices[2],
+          variable_Question_Choices[3]
+        ]
+      }
+    },
+    explanations: {
+      type: Array,
+      required: true,
+      default: function() {
+        return [
+          variable_Responses[0],
+          variable_Responses[1],
+          variable_Responses[2],
+          variable_Responses[3]
+        ]
+      }
+    },
+    correctChoice: {
+      type: Number,
+      required: true,
+      default: correct_Answer
+    }
+  },
+  data() {
+    return {
+      showAlert: false,
+      chosen: null,
+      msg: null,
+      correct: false,
+      submitDisabled: true
+    }
+  },
+  methods: {
+    onSubmit: function() {
+      this.correct = this.chosen === this.correctChoice
+      this.msg = this.correct ? "Correct. " : "Please try again. "
+      if (this.explanations[this.chosen]) {
+        this.msg += this.explanations[this.chosen]
+      }
+      this.showAlert = true
+    }
+  },
+  watch: {
+    chosen: function() {
+      this.showAlert = false
+      this.submitDisabled = false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.list-group-item .active {
+}
+</style>
