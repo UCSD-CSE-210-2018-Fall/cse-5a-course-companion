36f64462cd2defc2be9b53ea137b617bc1ae0409
Tommy Yang <yt940928@gmail.com>
2018-11-30 18:23:43 -0800

    formatting

--- frontend/tests/unit/FillBlank.spec.js
+++ frontend/tests/unit/FillBlank.spec.js
@@ -3,22 +3,27 @@ import { shallowMount } from "@vue/test-utils"
 import FillBlank from "@/components/FillBlank.vue"
 
 describe("FillBlank.vue", () => {
-	const wrapper = shallowMount(FillBlank, {
-            propsData: { question: "print hello world", code: "int i;\nint j = 6;\n", explanation: "hello world", correctAnswer: 3 }
-        })
+    const wrapper = shallowMount(FillBlank, {
+        propsData: {
+            question: "print hello world",
+            code: "int i;\nint j = 6;\n",
+            explanation: "hello world",
+            correctAnswer: 3
+        }
+    })
 
     it("renders", () => {
-        const mc_visible = wrapper.isVisible();
+        const mc_visible = wrapper.isVisible()
         mc_visible.should.be.equal(true)
     })
 
     it("question content", () => {
-		const question_p = wrapper.find("p");
-        question_p.text().should.be.equal("print hello world");
+        const question_p = wrapper.find("p")
+        question_p.text().should.be.equal("print hello world")
     })
 
     it("code content", () => {
-		const code = wrapper.find("code")
+        const code = wrapper.find("code")
         code.text().should.be.equal("int i;\nint j = 6;")
     })
 })
--- frontend/tests/unit/MultipleChoice.spec.js
+++ frontend/tests/unit/MultipleChoice.spec.js
@@ -3,22 +3,28 @@ import { shallowMount } from "@vue/test-utils"
 import MultipleChoice from "@/components/MultipleChoice.vue"
 
 describe("MultipleChoice.vue", () => {
-	const wrapper = shallowMount(MultipleChoice, {
-            propsData: { question: "print hello world", code: "int i;\nint j = 6;\n", choices: ["one","two","three","four"], explanation: ["one","two","three",null], correctAnswer: 3 }
-        })
+    const wrapper = shallowMount(MultipleChoice, {
+        propsData: {
+            question: "print hello world",
+            code: "int i;\nint j = 6;\n",
+            choices: ["one", "two", "three", "four"],
+            explanation: ["one", "two", "three", null],
+            correctAnswer: 3
+        }
+    })
 
     it("renders", () => {
-        const mc_visible = wrapper.isVisible();
+        const mc_visible = wrapper.isVisible()
         mc_visible.should.be.equal(true)
     })
 
     it("question content", () => {
-		const question_p = wrapper.find("p");
-        question_p.text().should.be.equal("print hello world");
+        const question_p = wrapper.find("p")
+        question_p.text().should.be.equal("print hello world")
     })
 
     it("code content", () => {
-		const code = wrapper.find("code")
+        const code = wrapper.find("code")
         code.text().should.be.equal("int i;\nint j = 6;")
     })
 })
